87d7a5b47adcfdf91add9f8d5b004641da666ac8
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-30 00:36:09 +0600

    Setting up forms.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-FONT = ("Calibri", 20, "bold")
+FONT = ("Calibri", 12, "bold")
 # ---------------------PASSWORD GENERATOR ----------------
 
 
@@ -22,10 +22,12 @@ web_name.config(text="Website", font=FONT)
 web_name.grid(row=1, column=0)
 
 web_entry = Entry()
-web_entry.config()
-web_entry.grid(row=1, column=1, columnspan=2)
+web_entry.config(width=35)
+web_entry.grid(row=1, column=0, columnspan=2)
 
-
+password_gen = Button()
+password_gen.config(text="Generate Password", font=FONT)
+password_gen.grid(row=3, column= 2)
 
 
 
