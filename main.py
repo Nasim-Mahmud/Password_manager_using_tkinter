7f22ba5f94532f0e8162be67c6ead7ec95bb3795
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-29 23:11:55 +0600

    Setting images.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ window.title("Password Manager")
 window.config(padx=100, pady=50)
 
 canvas = Canvas()
-lock_image = PhotoImage("logo.png")
+lock_image = PhotoImage(file="logo.png")
 canvas.create_image(256, 256, image=lock_image)
 canvas.grid(row=1, column=1)
 
