55e54a72f89060c03a35841b5dd5c7cd81eb0d95
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-30 00:27:37 +0600

    Setting images.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+FONT = ("Calibri", 20, "bold")
 # ---------------------PASSWORD GENERATOR ----------------
 
 
@@ -14,8 +14,11 @@ window.config(padx=20, pady=20)
 canvas = Canvas(height=200, width=200, highlightthickness=0)
 lock_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_image)
-canvas.grid(row=1, column=1)
+canvas.grid(row=0, column=1)
 
 # ------------------------FORM------------------------------
+web_name = Label()
+web_name.config(text="Website", font=FONT)
+web_name.grid(row=1, column=0)
 
 window.mainloop()
