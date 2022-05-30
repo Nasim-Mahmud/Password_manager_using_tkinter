1db41813ff14991e1cbb3440962d5be21ad493e0
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-30 13:40:16 +0600

    Setting up the UI.

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ FONT = ("Calibre", 10, "normal")
 # ---------------------UI SETUP --------------------------
 window = Tk()
 window.title("Password Manager")
-window.config(padx=40, pady=20)
+window.config(padx=40, pady=50)
 
 canvas = Canvas(height=200, width=200, highlightthickness=0)
 lock_image = PhotoImage(file="logo.png")
-canvas.create_image(130, 100, image=lock_image)
+canvas.create_image(123, 100, image=lock_image)
 canvas.grid(row=0, column=1, pady=20)
 
 # ------------------------FORM------------------------------
@@ -25,6 +25,7 @@ web_name.grid(row=1, column=0)
 
 web_entry = Entry()
 web_entry.config(width=58)
+web_entry.focus()
 web_entry.grid(row=1, column=1, columnspan=2, pady=5)
 
 email_name = Label()
