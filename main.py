268179e69d2e3cda574e582b902803a6728f4526
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-30 12:42:22 +0600

    Setting up forms.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ window.config(padx=20, pady=20)
 canvas = Canvas(height=200, width=200, highlightthickness=0)
 lock_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_image)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=1, pady=20)
 
 # ------------------------FORM------------------------------
 web_name = Label()
@@ -24,6 +24,14 @@ web_name.grid(row=1, column=0)
 web_entry = Entry()
 web_entry.config(width=50)
 web_entry.grid(row=1, column=1, columnspan=2)
+
+email_name = Label()
+email_name.config(text="Email/Username:", font=FONT)
+email_name.grid(row=2, column=0)
+
+email_entry = Entry()
+email_entry.config(width=50)
+email_entry.grid(row=2, column=1, columnspan=2)
 
 password_gen = Button()
 password_gen.config(text="Generate Password", font=FONT)
