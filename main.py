43db1f5522f95c809d1d3a4c7329376c4225ef17
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-30 13:17:07 +0600

    Setting up forms.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ web_name.config(text="Website:", font=FONT)
 web_name.grid(row=1, column=0)
 
 web_entry = Entry()
-web_entry.config(width=50)
+web_entry.config(width=58)
 web_entry.grid(row=1, column=1, columnspan=2)
 
 email_name = Label()
@@ -31,11 +31,11 @@ email_name.config(text="Email/Username:", font=FONT)
 email_name.grid(row=2, column=0)
 
 email_entry = Entry()
-email_entry.config(width=50)
+email_entry.config(width=58)
 email_entry.grid(row=2, column=1, columnspan=2)
 
 password_gen = Button()
-password_gen.config(text="Generate Password", font=FONT)
+password_gen.config(text="Generate Password", font=FONT, height=1)
 password_gen.grid(row=3, column=2)
 
 window.mainloop()
