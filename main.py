17c4df11f59090e225397f7abf27f5f53ce24eec
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-31 23:07:33 +0600

    Adding functionality to password generator button.

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import random
 
 FONT = ("Calibre", 10, "normal")
 
+
 # ---------------------PASSWORD GENERATOR ----------------
 def generate_password():
     # Password Generator Project
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -30,8 +33,8 @@ def generate_password():
     # for char in password_list:
     #     password += char
 
-    print(f"Your password is: {password}")
-
+    # print(f"Your password is: {password}")
+    password_entry.insert(f"{password}")
 
 # ---------------------SAVE PASSWORD----------------------
 def save():
@@ -92,7 +95,7 @@ password_entry.config(width=37)
 password_entry.grid(row=3, column=1, pady=5)
 
 password_gen_button = Button()
-password_gen_button.config(text="Generate Password", font=FONT, height=1)
+password_gen_button.config(text="Generate Password", font=FONT, height=1, command=generate_password)
 password_gen_button.grid(row=3, column=2, pady=5)
 
 add_button = Button()
