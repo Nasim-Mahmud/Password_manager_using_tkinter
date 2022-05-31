538b5b3bfc62846ea290f086c5426e5e148e4bac
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-31 23:01:51 +0600

    Importing password generator.

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
 password_list = password_letters + password_symbols + password_numbers
 random.shuffle(password_list)
 
-password = ""
-for char in password_list:
-    password += char
+password = "".join(password_list)
+
+# password = ""
+# for char in password_list:
+#     password += char
 
 print(f"Your password is: {password}")
 
